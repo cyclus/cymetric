c31c746f02041df0135d429f1b79a8ad36c4b04b
Olzhas <olzhasrakhimov7@gmail.com>
2014-02-17 05:11:11 -0600

    Add decay const table and clarify main script files.

---
+++ metrics.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+import query
+
+def main():
+    """Calculates nuclear fuel cycle metrics from cyclus output sql database.
+    """
+
+if __name__ == "__main__":
+    main()
