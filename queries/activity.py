6b24f50f927faf401451630851f7c8043fd5bfa6
Arrielle Christine Opotowsky <aopotowsky@dionysus.cae.wisc.edu>
2014-02-20 13:18:52 -0600

    Adding activities file

---
+++ queries/activity.py
@@ -0,0 +1,11 @@
+import sqlite3
+from pyne import data
+
+def waste_mass(c):
+    """Lists mass of waste transacted with respect to time.
+
+    Args:
+        c: connection cursor to sqlite database.
+    """
+    sql = """ """
+    return c.execute(sql)
