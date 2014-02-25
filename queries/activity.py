34217ca08e096357cbda7733cdf4fcb630f3c20e
Anthony Scopatz <scopatz@gmail.com>
2014-02-25 10:53:14 -0600

    have working version of actvity

--- queries/activity.py
+++ queries/activity.py
@@ -1,38 +1,25 @@
 import sqlite3
 from pyne import data
 
-
 CONV = 1000*6.022e23
 
-def activity(c, ):
+def activity(c):
     """Lists activities of all nuclides with respect to time for all facilities.
 
     Args:
         c: connection cursor to sqlite database.
     """
-    sql = """SELECT Resources.TimeCreated, Compositions.NucID, 
-                    Compositions.MassFrac*Resources.Quantity*""" 
-                    + CONV*data.decay_const("""Compositions.NucID""")/data.atomic_mass("""Compositions.NucID""") + 
-                    """
-             FROM Resources
-             INNER JOIN Compositions ON Resources.StateID = Compositions.StateID
-             GROUP BY Resources.TimeCreated, Compositions.NucID
-             ORDER BY Resources.TimeCreated;"""
-    return c.execute(sql)
-    
-def activity(c):
-    """Lists activities of all nuclides with respect to time for all HWRs.
-
-    Args:
-        c: connection cursor to sqlite database.
-    """
-    sql = """SELECT Resources.TimeCreated, Compositions.NucID, Compositions.MassFrac*Resources.Quantity*""" 
-                    + CONV*data.decay_const("""Compositions.NucID""")/data.atomic_mass("""Compositions.NucID""") + 
-                    """                    
-             FROM Resources
-             INNER JOIN Compositions ON Resources.StateID = Compositions.StateID
-             INNER JOIN Transactions ON  Resources.ResourceID = Transactions.ResourceID
-             WHERE Transactions.SenderID=21
-             GROUP BY Resources.TimeCreated, Compositions.NucID
-             ORDER BY Resources.TimeCreated;"""
-    return c.execute(sql)
+    sql = ("SELECT Resources.TimeCreated, Compositions.NucID," 
+           "Compositions.MassFrac*Resources.Quantity ")
+    sql += ("FROM Resources "
+            "INNER JOIN Compositions ON Resources.StateID = Compositions.StateID "
+            "GROUP BY Resources.TimeCreated, Compositions.NucID "
+            "ORDER BY Resources.TimeCreated;")
+    cur = c.execute(sql)
+    results = cur.fetchall()
+    activities = []
+    for time_step, nuc, mass in results:
+        act = CONV * data.decay_const(nuc) / data.atomic_mass(nuc)
+        row = (time_step, nuc, act)
+        activities.append(row)
+    return activities
