671305cc039d15d888a35dfb58a4190acd37741a
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2014-11-04 16:23:21 -0600

    clueless attempt at using cyclus API

--- main.cc
+++ main.cc
@@ -1,8 +1,10 @@
 #include <iostream>
-
+#include <cyclus.h>
 
 int main(int argc, char* argv[]) {
-  std::cout << "Hello World!\n";
-
+  std::cout << "Derp\n";
+  std::string table = "Compositions";
+  cyclus::QueryResult result = cyclus::QueryableBackend::Query(table, NULL);
   return 0;
 }
+
