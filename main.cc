70059bd8c814a4efb679a47cf5ff5273ce10c08f
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2014-11-11 13:50:56 -0600

    barebones main function

--- main.cc
+++ main.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cyclus.h>
-#include <hdf5_back.h>
 
 int main(int argc, char* argv[]) {
   using std::cout;
@@ -8,12 +7,6 @@ int main(int argc, char* argv[]) {
   cyclus::toolkit::ExponentialFunction* func = new cyclus::toolkit::ExponentialFunction(10.0, -1.0, 0.0);
   cout << func->Print() << "\n";
   delete func;
-  std::string table = "Compositions";
-  std::string fname = std::string(argv[1]);
-  cout << "file name: " << fname << "\n";
-  cyclus::FullBackend* fback = new cyclus::Hdf5Back(fname);
-  cyclus::QueryResult result = fback->Query(table, NULL);
-  delete fback;
   return 0;
 }
 
