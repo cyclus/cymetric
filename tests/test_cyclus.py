23af3f9dd8f794385cb9a04f4cb002d572aca92c
Anthony Scopatz <scopatz@gmail.com>
2014-12-22 13:23:27 -0800

    name() test

--- tests/test_cyclus.py
+++ tests/test_cyclus.py
@@ -1,7 +1,15 @@
 """Tests for cyclus wrappers"""
+import os
+import subprocess
+
 from nose.tools import assert_equal
 
 from cymetric import cyclus
+
+def setup():
+    if os.path.isfile('test.h5'):
+        return
+    subprocess.check_call(['cyclus', '-otest.h5', 'test-input.xml'])
 
 def test_hdf5_name():
     exp = "test.h5"
