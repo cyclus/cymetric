1548e1035cba248cecb98dc51101292176d2e93f
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2015-04-22 12:14:58 -0500

    adding pyne.enrichment import to test_metrics.py

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -15,6 +15,7 @@ from pandas.util.testing import assert_frame_equal
 
 try:
     from pyne import data
+    import pyne.enrichment as enr
     HAVE_PYNE = True
 except ImportError:
     HAVE_PYNE = False
