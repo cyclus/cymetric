2f096d8fc67f1c2c3e2d9ac41e1d70e9ee0c1162
MOUGINOT B <mouginot.baptiste@gmail.com>
2017-01-09 13:41:40 -0600

    start debug

--- cymetric/convenient_interface.py
+++ cymetric/convenient_interface.py
@@ -8,58 +8,54 @@ import warnings
 
 
 def merge_n_drop(pdf, base_col, add_pdf, add_col):
-        """
-        Merge some additionnal columns fram an additionnal Pandas Data Frame
-        onother one and then remove the second base column (keeping SimID
-        information).
-        Parameters
-        ----------
-        pdf : Pandas Data Frame
-        base_col : list of the base columns names
-        add_pdf : Pandas Data Frame to add in the pdf one
-        add_col : columns tobe added
-
-        Attributes
-        ----------
-        """
+    """
+    Merge some additionnal columns fram an additionnal Pandas Data Frame
+    onother one and then remove the second base column (keeping SimID
+    information).
+    Parameters
+    ----------
+    pdf : Pandas Data Frame
+    base_col : list of the base columns names
+    add_pdf : Pandas Data Frame to add in the pdf one
+    add_col : columns tobe added
+    """
     pdf = pd.merge(add_pdf[add_col], pdf, on=base_col)
     pdf.drop(base_col[1], 1)
     return pdf
 
 def get_reduced_pdf(pdf, rdc_list):
-        """
-        Filter the pdf Pandas Data Frame according to the rdc_list (list of item
-        in the corresponding columns).
-        Parameters
-        ----------
-        pdf : Pandas Data Frame
-        rdc_list : list of pair of string and string list.
-
-        Attributes
-        ----------
-        """
+    """
+    Filter the pdf Pandas Data Frame according to the rdc_list (list of item
+    in the corresponding columns).
+    Parameters
+    ----------
+    pdf : Pandas Data Frame
+    rdc_list : list of pair of string and string list.
+    """
     for rdc in rdc_list:
         if len(rdc[1]) != 0:
-            pdf = pdf.loc[compo[rdc[0].isin(rdc[1])]
+            print(rdc[0])
+            print(rdc[1])
+            pdf = pdf[ pdf[rdc[0]].isin(rdc[1]) ]
         else:
             wng_msg = "Empty list provided for " + rdc[0] + " key."
             warnings.wrn(wng_msg, UserWarning)
     return pdf
 
 def get_reduced__trans_pdf(db, send_name, rec_name):
-        """
-        Filter the Transaction Data Frame on specific sending facility and
-        receving facility.
+    """
+    Filter the Transaction Data Frame on specific sending facility and
+    receving facility.
 
-        Parameters
-        ----------
-        db : database
-        send_name : name of the sending facility ('All' for any)
-        rec_name : name of the receiving facility ('All for any)
+    Parameters
+    ----------
+    db : database
+    send_name : name of the sending facility ('All' for any)
+    rec_name : name of the receiving facility ('All for any)
 
-        Attributes
-        ----------
-        """
+    Attributes
+    ----------
+    """
 
     # initiate evaluation
     evaler = cym.Evaluator(db)
@@ -79,19 +75,20 @@ def get_reduced__trans_pdf(db, send_name, rec_name):
     if rec_list.empty or send_list.empty:
         return None
     else:
-        trans = get_reduced_pdf(trans, [[['ReceiverId',[rec_list.ReceiveriId]],
-                                        [['SenderId',[SenderId]]])
-        rsc = rsc.loc[rsc['ResourceId'].isin(trans.ResourceId)]
+        trans = get_reduced_pdf(trans, [
+            ['ReceiverId', rec_list['ReceiverId'].tolist() ],
+            ['SenderId', send_list['SenderId'].tolist() ] ])
+        rsc = rsc[ rsc['ResourceId'].isin( trans.ResourceId ) ]
 
         base_col = ['SimId', 'SenderId']
         added_col = base_col + ['Prototype']
         trans = merge_n_drop(trans, base_col, send_list, added_col)
-        trans = df.rename(index=str, columns={'Prototype': 'SenderProto'})
+        trans = trans.rename(index=str, columns={'Prototype': 'SenderProto'})
 
         base_col = ['SimId', 'ReceiverId']
         added_col = base_col + ['Prototype']
         trans = merge_n_drop(trans, base_col, send_list, added_col)
-        trans = df.rename(index=str, columns={'Prototype': 'ReceiverProto'})
+        trans = trans.rename(index=str, columns={'Prototype': 'ReceiverProto'})
 
         base_col = ['SimId', 'RessourceId']
         added_col = base_col + ['QualId', 'Quantity', 'Unit']
@@ -100,23 +97,20 @@ def get_reduced__trans_pdf(db, send_name, rec_name):
     return trans
 
 
-def get_transaction_timeseries(db, send_name='All', rec_name='All', nuc_list):
-        """
-        Shape the reduced transation Dta Frame into a simple time serie. Apply
-        some nuclei selection if required.
+def get_transaction_timeseries(db, send_name='All', rec_name='All', nuc_list=[]):
+    """
+    Shape the reduced transation Dta Frame into a simple time serie. Apply
+    some nuclei selection if required.
 
-        Parameters
-        ----------
-        db : database
-        send_name : name of the sending facility
-        rec_name : name of the receiving facility
-        nuc_list : list of nuclide to select.
+    Parameters
+    ----------
+    db : database
+    send_name : name of the sending facility
+    rec_name : name of the receiving facility
+    nuc_list : list of nuclide to select.
+    """
 
-        Attributes
-        ----------
-        """
-
-    df = get_transaction_timeseries(db, send_name, rec_name)
+    df = get_reduced__trans_pdf(db, send_name, rec_name)
 
     if len(nuc_list) != 0:
         compo = evaler.eval('Compositions')
@@ -152,20 +146,17 @@ def get_transaction_timeseries(db, send_name='All', rec_name='All', nuc_list):
     return trans_table
 
 
-def GetInventoryTimeSeries(db, fac_name, nuc_list):
-        """
-        Shape the reduced inventory Dta Frame into a simple time serie. Apply
-        some nuclei selection if required.
+def get_inventory_timeseries(db, fac_name, nuc_list):
+    """
+    Shape the reduced inventory Dta Frame into a simple time serie. Apply
+    some nuclei selection if required.
 
-        Parameters
-        ----------
-        db : database
-        fac_name : name of the facility
-        nuc_list : list of nuclide to select.
-
-        Attributes
-        ----------
-        """
+    Parameters
+    ----------
+    db : database
+    fac_name : name of the facility
+    nuc_list : list of nuclide to select.
+    """
     evaler = cym.Evaluator(db)
 
     # Get inventory table
